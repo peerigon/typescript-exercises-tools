b7e4c3cf79ca821afa8e88d9a8ec6462567eac42
Johannes Ewald <johannes.ewald@peerigon.com>
2022-12-20 12:10:18 +0100

    chore: Ignore import/no-unresolved in plugin.js

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
 // This file is currently necessary because the TypeScript language server requires the
 // plugin to be a CommonJS module which exports a function as module.exports
-// Just pointing to "./dist/plugin.js" is not enough because the compiled module
+// eslint-disable-next-line import/no-unresolved
 module.exports = require("./dist/plugin.js").default;
