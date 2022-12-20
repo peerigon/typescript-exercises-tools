6a4f6077a1e2d165f64fe9cb3684104854b20447
Johannes Ewald <johannes.ewald@peerigon.com>
2022-12-20 13:58:09 +0100

    chore: Adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    // Removes the .js extension in imports
+    // Based on recommendation in https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
